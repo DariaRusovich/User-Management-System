095266e255aad131e5ed55f7b4730910843b19b7
DariaRusovich <rusovichdasha41@gmail.com>
2022-01-11 12:38:05 +0200

    delete-useless-files

--- src/App.js
+++ src/App.js
@@ -1,38 +1,12 @@
 import { React, Component } from 'react';
-import { Route, Switch, Link } from 'react-router-dom';
-import DepartmentsList from './components/DepartmentsList';
-import EmployeesList from './components/EmployeesList';
-import Error from './components/Error';
-import Header from './components/Header';
+
+
 
 export default class App extends Component {
   render() {
     return (
       <>
-      <Header></Header>
-        <div className="App">
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/employees">EmployeesList</Link>
-              </li>
-            </ul>
-          </nav>
-          <Switch>
-            <Route path="/employees">
-              <EmployeesList></EmployeesList>
-            </Route>
-            <Route exact path="/">
-              <DepartmentsList></DepartmentsList>
-            </Route>
-            <Route path="*">
-              <Error />
-            </Route>
-          </Switch>
-        </div>
+     
       </>
     );
   }
